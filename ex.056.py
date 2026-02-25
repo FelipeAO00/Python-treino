d28ec37ae1f3eb02f10e4c645e72dae00116c18c
Felipe <felipe1rj@hotmail.com>
2026-02-25 12:27:58 -0300

    Arquivos novos

---
+++ ex.056.py
@@ -0,0 +1,16 @@
+#Exercício Python 056 - Analisador completo
+
+
+
+
+print("----1 pessoa ----")
+NomeP1 = input("Nome: ")
+
+
+print("----2 pessoa ----")
+
+
+print("----3 pessoa ----")
+
+
+print("----4 pessoa ----")
